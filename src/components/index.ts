8710a5233d19075634ac32662337290c0e17e88d
tchalla <sssillah1@gmail.com>
2023-11-23 09:02:19 +0900

    Fix legacy deps and Update blocktype index
    Building with webpack was incompatible with current node version due to an update in ssl hash. Using legacy provider fixed this issue.
    
    When trying to hit /blog/posts/:slug, met with an error. After debugging, there was a typescript error in the blogpost, where it couldn't index with BlockTypes due to paragraph being missing. Added a paragraph component, which fixed the typeing issue and sent the data to the dom
     main

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import { Blockquote } from "./blockquote";
 import { Header } from "./header";
 import { Image } from "./image";
+import { Paragraph } from "./paragraph";
 
 export const components = {
   BLOCKQUOTE: Blockquote,
   HEADER: Header,
   IMAGE: Image,
+  PARAGRAPH: Paragraph
 };
